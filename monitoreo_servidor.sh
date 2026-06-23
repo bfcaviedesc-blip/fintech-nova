ea85b0caf92acda43eef83e98475b75f6c18554c
bfcaviedesc-blip <bfcaviedesc@sanmateo.edu.co>
2026-06-23 06:55:11 +0000

    Se añadió script para monitoreo del estado del servidor

---
+++ monitoreo_servidor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+UMBRALCPU=80
+UMBRALRAM=80
+UMBRALDISCO=85
+
+USORAM=$(free | grep Mem | awk '{print int (($3 / $2)*100)}')
+
+if [ "$USORAM" -ge "$UMBRALRAM" ]; then
+	echo "WARNING, USO ELEVADO DE RAM. Uso actual: $USORAM%"
+fi	
+if [ "$USORAM" -le "$UMBRALRAM" ]; then
+	echo "USO DE RAM NORMAL. Uso actual: $USORAM%"
+fi
+
+USODISCO=$(df -h . | tail -1 | awk '{print $5}' | tr -d '%')
+
+if [ "$USODISCO" -ge "$UMBRALDISCO" ]; then
+	echo "WARNING, DISCO CASI LLENO. Uso actual: $USODISCO"
+fi
+if [ "$USODISCO" -le "$UMBRALDISCO" ]; then
+	echo "ESPACIO DISPONIBLE: $USODISCO"
+fi
+
+USOCPU=$(top -b -d 0.5 -n 5 | grep Cpu | tail -1 | awk '{print int(100-$8)}')
+
+if [ "$USOCPU" -ge "$UMBRALCPU" ]; then
+	echo "WARNING, uso alto de CPU. Uso actual: $USOCPU%"
+fi
+if [ "$USOCPU" -le "$UMBRALCPU" ]; then
+	echo "Uso de CPU actual: $USOCPU%"
+fi
