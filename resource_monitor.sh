74a02a26a54b819196fdbdbfb5286cf6bb494d98
bfcaviedesc-blip <bfcaviedesc@sanmateo.edu.co>
2026-06-23 07:15:45 +0000

    actualización del nombre del script de monitoreo

---
+++ resource_monitor.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Definición de limites
+
+UMBRALCPU="80"
+UMBRALRAM="80"
+UMBRALDISCO="85"
+
+# Fecha de la consulta
+echo "[$(date '+%Y/%m/%d %H:%M:%S')] Consulta estado del servidor"
+
+# Definición del uso de la RAM
+USORAM=$(free | grep Mem | awk '{print int (($3 / $2)*100)}')
+
+# Respuesta que se debe imprimir
+
+if [ "$USORAM" -ge "$UMBRALRAM" ]; then
+	echo "WARNING, USO ELEVADO DE RAM: $USORAM%"
+fi	
+if [ "$USORAM" -le "$UMBRALRAM" ]; then
+	echo "USO DE RAM ACTUAL: $USORAM%"
+fi
+
+# Definición del uso del disco
+
+USODISCO=$(df -h . | tail -1 | awk '{print $5}' | tr -d '%')
+
+# Respuesta que imprime el disco
+if [ "$USODISCO" -ge "$UMBRALDISCO" ]; then
+	echo "WARNING, DISCO CASI LLENO: $USODISCO%"
+fi
+if [ "$USODISCO" -le "$UMBRALDISCO" ]; then
+	echo "USO DE ALMACENAMIENTO ACTUAL: $USODISCO%"
+fi
+
+# Definición del uso de la CPU
+
+USOCPU=$(top -b -d 0.5 -n 5 | grep Cpu | tail -1 | awk '{print int(100-$8)}')
+
+# Respuesta que se imprime del uso del CPU
+if [ "$USOCPU" -ge "$UMBRALCPU" ]; then
+	echo "WARNING, USO ALTO DE LA CPU: $USOCPU%"
+fi
+if [ "$USOCPU" -le "$UMBRALCPU" ]; then
+	echo "USO DE CPU: $USOCPU%"
+fi
